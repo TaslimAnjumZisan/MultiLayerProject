52a2a1a91e1cc0b9106c57f279f0c488adb52555
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-07-06 17:43:07 +0600

    DLL BOL DAL

---
+++ DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjectLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=.;Database=AspMultiLayerArc;Trusted_Connection=True;TrustServerCertificate=True");
+
+        }
+        public DbSet<Categorys> Categories { get; set; }
+    }
+}
